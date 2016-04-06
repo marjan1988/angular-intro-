3acc08e43187f53674bd124a745f46a86403fe34
Marjan Maric <marjan.maric1988@gmail.com>
2016-04-06 10:48:06 +0200

    backend input setup - look at console

--- js/app.js
+++ js/app.js
@@ -3,12 +3,21 @@ var myApp = angular.module('myApp', []);
 //Created Controller
 myApp.controller('MainCtrl', function($scope){
 	
+	$scope.users = [];
 	$scope.user = {};
 	
 	$scope.addUser = function(){
 		
-		console.log($scope.user);
+		var newUser = {
+			
+			name     : $scope.user.name,
+			surename : $scope.user.surename
+			
+		};
 		
+		$scope.users.push(newUser);
+		
+		console.log($scope.users);
 	};
 	
 });
